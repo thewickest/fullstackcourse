e6693e971e9f02cf4100cf232446231ef65afe64
The Wickest <graulomail>
2022-02-16 13:40:03 +0100

    Exercise 1.4 from part 1

--- part1/src/index.js
+++ part1/src/index.js
@@ -10,6 +10,7 @@ const Header = (prop) => {
 }
 
 const Content = (prop) => {
+  console.log(prop.part)
   return (
     <div>
       <Part part={prop.part[0]}/>
@@ -38,23 +39,25 @@ const Total = (prop) =>{
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   return (
     <div>
       <Header name={course} />
-      <Content part={[part1,part2,part3]}/>
-      <Total total={part1.exercises + part2.exercises + part3.exercises}/>
+      <Content part={parts}/>
+      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
     </div>
   )
 }
