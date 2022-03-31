d6a77e2ba7b4043cdac26cb75e59f4737d11d2c7
The Wickest <graubuntu@gmail.com>
2022-03-31 19:37:10 +0200

    Exercise 3.2 from part 3

--- part3/phonebook-server/index.js
+++ part3/phonebook-server/index.js
@@ -1,35 +1,43 @@
+const { application } = require('express')
 const express = require('express')
 
 app = express()
 
 const persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
-app.get('/api/persons',(request,response)=>{
+app.get('/api/persons', (request, response) => {
     response.send(persons)
 })
 
+app.get('/info', (request, response) => {
+    response.send(
+        `<p>Phonebook has info por ${persons.length} people</p>`
+        +`<p>${new Date()}</p>`
+    )
+})
+
 const PORT = 3001
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server running in port...${PORT}`)
 })
