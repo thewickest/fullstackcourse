da1185150f4ccb144f420d171a8f4cb3246f7ff7
The Wickest <graubuntu@gmail.com>
2022-04-05 19:11:29 +0200

    Exercise 3.12 from part 3

---
+++ part3/phonebook-server/mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+const args = process.argv
+
+const password = args[2]
+
+if (args.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+
+const url = 
+    `mongodb+srv://graubuntu:${password}@cluster0.yttzt.mongodb.net/phoneBookDB?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person',personSchema)
+
+if(args.length==3){
+    Person.find({}).then(result =>{
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}else{
+    const person = new Person({
+        name: args[3],
+        number: args[4]
+    })
+    person.save().then(result =>{
+        console.log(`added ${result.name} number ${result.number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
