6625380bc7f946528db6fc7def3bdae2a04a4fb5
The Wickest <graubuntu@gmail.com>
2022-05-05 23:01:00 +0200

    Exercise 4.5 from part 4: Favorite Blog

--- part4/blog/backend/utils/list_helper.js
+++ part4/blog/backend/utils/list_helper.js
@@ -1,7 +1,17 @@
 const dummy = (blogs) => 1
 
-const totalLikes = (blogs) => blogs.reduce((sum,blog)=>sum+blog.likes,0)
+const totalLikes = (blogs) => blogs.reduce((sum,blog) => sum+blog.likes,0)
+
+const favoriteBlog = (blogs) => {
+    return (blogs.length===0) 
+        ? {} 
+        : blogs.reduce((max,blog) => 
+            (blog.likes > max.likes) 
+                ? ({ title: blog.title,author: blog.author,likes: blog.likes }) 
+                : max
+        ,{likes:0})
+}
 
 module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
 }
