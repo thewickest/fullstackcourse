8f134e92c4884ed6da2aadb8175c02211d40f73b
The Wickest <graubuntu@gmail.com>
2022-05-06 14:05:30 +0200

    Exercise 4.7 from part 4: Most liked author

--- part4/blog/backend/utils/list_helper.js
+++ part4/blog/backend/utils/list_helper.js
@@ -17,12 +17,25 @@ const favoriteBlog = (blogs) => {
 const mostBlogs = (blogs) => {
     if (blogs.length===0) return {}
     else{
-    const gBlogs = lodash.groupBy(blogs, (each) => each.author)
-    const mostBlog = lodash.orderBy(gBlogs,gBlogs.length,'desc')[0]
-    return {author:mostBlog[0].author, blogs:mostBlog.length}
+        const gBlogs = lodash.groupBy(blogs, (each) => each.author)
+        const mostBlog = lodash.orderBy(gBlogs,gBlogs.length,'desc')[0]
+        return {author:mostBlog[0].author, blogs:mostBlog.length}
+    }
+}
+
+const mostLikes = (blogs) => {
+    if (blogs.length ===0) return {}
+    else{
+        const gBlogs = lodash.groupBy(blogs, (each) => each.author)
+        const likesBlogs = Object.keys(gBlogs).map(
+            (key)=>
+                gBlogs[key].reduce(
+                    (sum,blog)=> ({author:blog.author,likes:sum.likes+blog.likes})
+                    ,{author:"dummy",likes:0}))
+        return lodash.orderBy(likesBlogs, 'likes','desc')[0]
     }
 }
 
 module.exports = {
-    dummy, totalLikes, favoriteBlog, mostBlogs
+    dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
 }
