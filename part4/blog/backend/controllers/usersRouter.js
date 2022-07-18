8a57530134c4cf5329b5f779cd612c79d465058b
The Wickest <graubuntu@gmail.com>
2022-07-18 13:43:22 +0200

    RE-DONE:   Part2

--- part4/blog/backend/controllers/usersRouter.js
+++ part4/blog/backend/controllers/usersRouter.js
@@ -4,7 +4,7 @@ const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
     const users = await User
-        .find({}).populate('blogs', {title: 'test'})
+        .find({})
     response.json(users)
 })
 
